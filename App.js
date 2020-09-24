f195dc337e812e93023a1b3d1574c11d0e507346
trhaiahiho <haitn@ahiho.com>
2020-09-24 10:36:08 +0700

    create socket.io server

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 var port = 3000;
 app.use(express.static('public'));
 app.set("view engine", "ejs");
@@ -9,6 +10,16 @@ server.listen(port, () => {
   console.log('Server listening', port)
 });
 
-app.get("/",  (req, res) => {
+io.on('connection', socket => {
+
+  console.log(socket.id + " connecting...");
+
+  socket.on('disconnect', () => {
+
+    console.log(socket.id + " disconnected.");
+
+  });
+});
+app.get("/", (req, res) => {
   res.render("home");
 });
